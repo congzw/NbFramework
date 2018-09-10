294ea6142a7f9c2e369def79cb514f5587b1afc2
congzw <2981810300@qq.com>
2018-09-10 09:59:54 +0800

    add unit test for TextRules

---
+++ src/NbFramework.UnitTests/TextRules/TextRuleEngineSpecs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NbFramework.TextRules.Core;
+
+namespace NbFramework.TextRules
+{
+    [TestClass]
+    public class TextRuleEngineSpecs
+    {
+        [TestMethod]
+        public void not_expire_date_should_ok()
+        {
+            var engine = TextEngineFactory.Create();
+            engine.GetValidateContext = () => new TextRuleValidateContext()
+            {
+                GetMachineCode = () => "ABC",
+                GetNow = () => new DateTime(2000, 1, 1),
+                GetRequestContext = () => new Dictionary<string, object>()
+            };
+
+            var vr = engine.Validate_ApplicationStart();
+            Console.WriteLine(vr.Message);
+            Assert.IsTrue(vr.Success);
+        }
+
+        [TestMethod]
+        public void expire_date_should_failed()
+        {
+            var engine = TextEngineFactory.Create();
+            engine.GetValidateContext = () => new TextRuleValidateContext()
+            {
+                GetMachineCode = () => "ABC",
+                GetNow = () => new DateTime(2000, 2, 1),
+                GetRequestContext = () => new Dictionary<string, object>()
+            };
+
+            var vr = engine.Validate_ApplicationStart();
+            Console.WriteLine(vr.Message);
+            Assert.IsFalse(vr.Success);
+        }
+    }
+}
---
+++ src/NbFramework.UnitTests/TextRules/_Mocks.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NbFramework.TextRules.Core;
+using NbFramework.TextRules.Core.Validators;
+
+namespace NbFramework.TextRules
+{
+    public class TextEngineFactory
+    {
+        public static TextRuleEngine Create()
+        {
+            var engine = new TextRuleEngine
+            {
+                Rules = LoadRules(),
+                Validators = GetValidators()
+            };
+            return engine;
+        }
+
+        private static IList<TextRule> LoadRules()
+        {
+            var rules = new List<TextRule>();
+            rules.Add(new TextRule() { Name = TextRuleNames.Customer, Value = "Foo", Scope = "" });
+            rules.Add(new TextRule() { Name = TextRuleNames.MachineCode, Value = "abc", Scope = "" });
+            rules.Add(new TextRule() { Name = TextRuleNames.SiteCount, Value = "3", Scope = "" });
+            rules.Add(new TextRule() { Name = TextRuleNames.DateExpired, Value = "2000-01-01", Scope = "" });
+            rules.Add(new TextRule() { Name = TextRuleNames.DateExpired, Value = "2000-02-01", Scope = "SiteA" });
+            rules.Add(new TextRule() { Name = TextRuleNames.DateExpired, Value = "2000-03-01", Scope = "SiteB,SiteC" });
+            rules.Add(new TextRule() { Name = TextRuleNames.V2EntryCount, Value = "3", Scope = "" });
+            return rules;
+        }
+        private static IList<ITextRuleValidator> GetValidators()
+        {
+            var validators = new List<ITextRuleValidator>();
+            validators.Add(new DateExpiredValidator());
+            return validators;
+        }
+
+    }
+
+    public class MockTextValidateContext
+    {
+        public DateTime GetNow()
+        {
+            return new DateTime(2000, 1, 1);
+        }
+
+        public string GetMachineCode()
+        {
+            return "ABC";
+        }
+
+        public IDictionary<string, object> GetRequestContext()
+        {
+            return new Dictionary<string, object>();
+        }
+    }
+}
