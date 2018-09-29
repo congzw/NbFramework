eb03fc0da9fd7318e024bef9b977885a9ef2bab9
congzw <2981810300@qq.com>
2018-09-29 10:12:58 +0800

    add areaLoader plugin demo

---
+++ src/DemoSite/Areas/Foo/content/scripts/hello.js
@@ -0,0 +1,7 @@
+﻿define(['nbLog'], function (nbLog) {
+    nbLog.debug("loading hello.js");
+
+    return {
+        say: "hello world!"
+    };
+});
---
+++ src/DemoSite/Content/libs/areaLoader.js
@@ -0,0 +1,19 @@
+﻿define({
+    load: function (name, req, onload, config) {
+        var split = name.split('/');
+        var area = split[0];
+        var componnetParts = split.slice(1);
+        var component = componnetParts.join('/');
+        //append '.js' by check necessary  => todo
+
+        var fixName = '/areas/' + area + '/content/scripts/' + component + '.js';
+        //console.log(area);
+        //console.log(componnetParts);
+        //console.log(component);
+        //console.log(name);
+        //console.log(fixName);
+        req([fixName], function (value) {
+            onload(value);
+        });
+    }
+});
