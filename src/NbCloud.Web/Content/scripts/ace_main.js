f59bf919377e0b3e635feea4b1ad0c870b43d1b8
congzw <2981810300@qq.com>
2018-10-10 10:54:00 +0800

    add ace support

---
+++ src/NbCloud.Web/Content/scripts/ace_main.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    "use strict";
+    require(['app_const'], function (app_const) {
+
+        //console.log("enter ace_main => ");
+        //console.log(app_const);
+        var rootPath = app_const.rootPath;
+        var contentPath = app_const.contentPath;
+        var acePath = app_const.acePath;
+
+        require.config({
+            baseUrl: rootPath,
+            waitSeconds: 2000,
+            paths: {
+                'jquery': acePath + '/components/jquery/dist/jquery.min'
+                , 'bootstrap': acePath + '/components/bootstrap/dist/js/bootstrap.min'
+                , 'ace': acePath + '/assets/js/ace'
+                , 'ace-elements': acePath + '/assets/js/ace-elements'
+                , 'ace-extra': acePath + '/assets/js/ace-extra'
+                , 'ace_main': contentPath + '/scripts/ace_main'
+                , 'ace_nav': contentPath + '/scripts/ace_nav'
+            },
+            shim: {
+                'bootstrap': {
+                    deps: ['jquery']
+                },
+                'ace': {
+                    deps: ['jquery', 'bootstrap']
+                },
+                'ace-elements': {
+                    deps: ['ace']
+                }
+            }
+        });
+        
+        require(['jquery', 'bootstrap', 'ace', 'ace-extra', 'ace-elements'], function ($) {
+            try {
+                ace.demo.init(true);//true means the call is not from inside a jQuery document ready event
+            } catch (e) { }
+
+            /**
+            //you can restore state of elements now (ace-extra.js is loaded)
+            $('.ace-save-state').each(function() {
+                ace.settings.loadState(this);
+            });
+    
+            //or as soon as you render an element such as sidebar, etc ...
+              ace.settings.loadState('sidebar');
+            */
+        });
+    });
+})();
---
+++ src/NbCloud.Web/Content/scripts/ace_nav.js
@@ -0,0 +1,45 @@
+﻿define(["jquery"], function ($) {
+    "use strict";
+
+    var activeNavItem = function(currentActiveId, openAll) {
+
+        //somehow get a reference to our newly clicked(selected) element's parent "LI"
+
+        var $current = $("#" + currentActiveId);
+        var new_active = $current.parent();
+
+        if (openAll) {
+            //打开所有的菜单
+            $('.nav-list li').addClass('open');
+        }
+
+        //remove ".active" class from all (previously) ".active" elements
+        $('.nav-list li.active').removeClass('active hover');
+
+        //add ".active" class to our newly selected item and all its parent "LI" elements
+        new_active.addClass('active open hover').parents('.nav-list li').addClass('active open');
+
+        //you can also update breadcrumbs:
+        var breadcrumb_items = [];
+        //$(this) is a reference to our clicked/selected element
+        $current.parents('.nav-list li').each(function() {
+            var link = $(this).find('> a');
+            var text = link.text();
+            var href = link.attr('href');
+            breadcrumb_items.push({ 'text': text, 'href': href });
+        });
+        //now we have a breadcrumbs list and can replace breadcrumbs area
+        return breadcrumb_items;
+    };
+
+    var updateBreadcrumbs = function (breadcrumbs) {
+        //todo
+        console.log('todo updateBreadcrumbs');
+        console.log(breadcrumbs);
+    };
+
+    return {
+        activeNavItem: activeNavItem,
+        updateBreadcrumbs: updateBreadcrumbs
+    };
+});
