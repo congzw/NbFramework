85d2152a093a40ca768e0072f3bc853acec6f4fe
congzw <2981810300@qq.com>
2018-10-22 17:07:12 +0800

    add missing file

---
+++ src/NbCloud.Web/Areas/Auth/AppServices/IAccountAppService.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NbFramework.Common;
+using NbFramework.Common.AppServices;
+
+namespace NbCloud.Web.Areas.Auth.AppServices
+{
+    public interface IAccountAppService : IAppService
+    {
+        MessageResult ValidateLogin(LoginVo vo);
+    }
+
+    public class LoginVo
+    {
+        [Display(Name = "User name")]
+        [Required]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+    }
+}
